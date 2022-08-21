14cd5b8722ebe52daca8b1268eadcb2890674510
oshita-n <oshita-n@whispon.com>
2022-08-21 16:00:27 +0900

    🔨 文字のフォントの調整

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react';
 export const Header: FC = () => {
   return (
     <div>
-      <header className='flex w-screen h-[65px] items-center justify-center'>
+      <header className='flex w-screen h-[65px] items-center justify-center font-bold'>
         <h1 className='ml-5'>Shimabu IT University</h1>
         <ul className='flex ml-auto mr-5'>
             <li className='ml-2'><a href='#'>About</a></li>
