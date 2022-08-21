5ccf7229adb142fab4e159c55ec7ca1aa86afb42
oshita-n <oshita-n@whispon.com>
2022-08-21 16:41:58 +0900

    🔨 githubコンポーネントの追加

---
+++ src/components/github/Github.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+
+export const Github: FC = () => {
+  return (
+    <div>
+        <div className='border-b border-[#E9ECEF] mt-[80px]'>
+            <h1 className='font-bold mb-[20px]'>Github</h1>
+        </div>
+    </div>
+  );
+};
