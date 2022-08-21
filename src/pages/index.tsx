0258a767637cf9135a28f7fee1836bfcce3ca803
oshita-n <oshita-n@whispon.com>
2022-08-21 13:59:32 +0900

    🔨 ヘッダーの調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 
-import { Footer } from '../components/Footer'
+import { Footer } from '../layout/Footer'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
@@ -12,12 +12,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className='w-screen h-16 bg-sky-400'>
-        <ul>
-          <li><a href='#'>About</a></li>
-          <li><a href='#'>Blog</a></li>
-          <li><a href='#'>Portfolio</a></li>
-          <li><a href='#'>Contact</a></li>
+      <header className='flex w-screen h-16 bg-sky-400'>
+        <h1 className='text-left mt-5 ml-5'>Shimabu IT University</h1>
+        <ul className='flex ml-auto mt-5 mr-5'>
+            <li className='ml-2'><a href='#'>About</a></li>
+            <li className='ml-2'><a href='#'>Blog</a></li>
+            <li className='ml-2'><a href='#'>Portfolio</a></li>
+            <li className='ml-2'><a href='#'>Contact</a></li>
         </ul>
       </header>
 
