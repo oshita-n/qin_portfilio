587fede73076c6aaa0cb27a3d72fbf33ed001114
oshita-n <oshita-n@whispon.com>
2022-09-04 04:18:13 +0900

    🔨 aboutページの作成

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -6,7 +6,7 @@ export const Header: FC = () => {
       <header className='flex w-screen h-[65px] items-center justify-center font-bold'>
         <h1 className='ml-5'>Shimabu IT University</h1>
         <ul className='flex ml-auto mr-5'>
-            <li className='ml-2'><a href='#'>About</a></li>
+            <li className='ml-2'><a href='/about'>About</a></li>
             <li className='ml-2'><a href='#'>Blog</a></li>
             <li className='ml-2'><a href='#'>Portfolio</a></li>
             <li className='ml-2'><a href='#'>Contact</a></li>
