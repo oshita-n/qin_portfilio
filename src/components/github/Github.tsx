45357f024653fe14c3fdfc452b244c2c01dd8e94
oshita-n <oshita-n@whispon.com>
2022-09-26 21:52:31 +0900

    🐛 Githubコンポーネントの修正

--- src/components/github/Github.tsx
+++ src/components/github/Github.tsx
@@ -6,10 +6,10 @@ export const Github: FC = () => {
     <div>
         <div className='border-b border-[#E9ECEF] mt-[80px]'>
             <h1 className='font-bold mb-[20px]'>Github</h1>
-            <GithubItem />
-            <div className="text-center">
-                <button className="bg-[#25262B] text-white px-4 py-2 rounded-full mt-2 mb-2">View All</button>
-            </div>
+        </div>
+        <GithubItem />
+        <div className="text-center">
+            <button className="bg-[#25262B] text-white px-4 py-2 rounded-full mt-2 mb-2">View All</button>
         </div>
     </div>
   );
