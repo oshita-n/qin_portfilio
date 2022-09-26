0574eaed9d0b9469b3a8a3081e662bf1aaedcc23
oshita-n <oshita-n@whispon.com>
2022-09-26 22:03:31 +0900

    ⚡️ Linkコンポーネントへ置き換え

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,16 @@
 import { FC, ReactNode } from 'react';
+import Link from 'next/link';
 
 export const Header: FC = () => {
   return (
     <div>
       <header className='flex w-screen h-[65px] items-center justify-center font-bold'>
-        <li className="list-none"><a href="/"><h1 className='ml-5'>Shimabu IT University</h1></a></li>
+        <li className="list-none"><Link href="/"><h1 className='ml-5'>Shimabu IT University</h1></Link></li>
         <ul className='flex ml-auto mr-5'>
-            <li className='ml-2'><a href='/about'>About</a></li>
-            <li className='ml-2'><a href='#'>Blog</a></li>
-            <li className='ml-2'><a href='#'>Portfolio</a></li>
-            <li className='ml-2'><a href='/contact'>Contact</a></li>
+            <li className='ml-2'><Link href='/about'>About</Link></li>
+            <li className='ml-2'><Link href='#'>Blog</Link></li>
+            <li className='ml-2'><Link href='#'>Portfolio</Link></li>
+            <li className='ml-2'><Link href='/contact'>Contact</Link></li>
         </ul>
       </header>
     </div>
